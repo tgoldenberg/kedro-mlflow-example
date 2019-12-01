4126b38f46e074a46076f1ea2e4861f7941ea6b1
tgoldenberg <thomasagoldenberg@gmail.com>
2019-12-01 03:11:56 -0700

    get MLflow project working successfully

---
+++ config-mlflow.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export MLFLOW_CONDA_HOME="/Users/Tom_Goldenberg/anaconda3"
+export MLFLOW_TRACKING_URI="http://0.0.0.0:5000"
+#export MLFLOW_TRACKING_URI="/Users/Tom_Goldenberg/Desktop/Projects/kedro-mlflow-example/mlruns"
+export MLFLOW_EXPERIMENT_NAME="iris-example"
